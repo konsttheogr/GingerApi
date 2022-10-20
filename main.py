40de316440356cfd3229a44a83f7a51baf2b84d1
KonstTheoGR <60752156+konsttheogr@users.noreply.github.com>
2022-10-20 18:00:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ def login():
     # the code below is executed if the request method
     # was GET or the credentials were invalid
     return render_template('login.html', error=error)
+
+if __name__ == '__main__':
+    app.run()
     
 """   
 
