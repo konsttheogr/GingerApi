0f640c86ab8d42a92c87168fe4427f5fd38526e3
KonstTheoGR <60752156+konsttheogr@users.noreply.github.com>
2022-10-21 17:23:56 +0300

    chganged test api

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("https://flask.palletsprojects.com/en/2.2.x/quickstart/#a-minimal-applicat
 
 psw = os.environ['psw']
 @app.route(f"/{psw}/")
-def hello_world(name='Uh idk you didn\'t mention it'):
+def hello_world(name):
     #return f"<p>Hello, {name}!</p>"
     return render_template('test.html', name=name)
     
