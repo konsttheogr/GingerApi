fbb3fa985bcdcf44476fc67bfd5985fcd4ac67fb
KonstTheoGR <60752156+konsttheogr@users.noreply.github.com>
2022-10-21 17:47:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("https://flask.palletsprojects.com/en/2.2.x/quickstart/#a-minimal-applicat
 
 psw = os.environ['psw']
 @app.route(f"/{psw}/")
-def hello_world(name):
+def hello_world(name="Idk"):
     #return f"<p>Hello, {name}!</p>"
     return render_template('test.html', name=name)
     
